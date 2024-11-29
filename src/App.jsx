efbd36dcccd057ac1d748f13a2a9dca858a35444
kunaldhuria95 <121857788+kunaldhuria95@users.noreply.github.com>
2024-11-29 21:29:40 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Home from './pages/home/Home';
 import Navbar from './components/Navbar/Navbar';
 import Login from './pages/login/Login';
 import './styles/global.css'
-import Item from './pages/item/item.jsx';
+import Item from './pages/item/Item';
 
 function App() {
 
