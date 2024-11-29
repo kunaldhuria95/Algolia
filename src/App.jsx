b348c175a0c71281930bf9eddcbcaec8469ddf25
kunaldhuria95 <121857788+kunaldhuria95@users.noreply.github.com>
2024-11-29 21:26:58 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import {
 } from "react-router-dom";
 import Home from './pages/home/Home';
 import Navbar from './components/Navbar/Navbar';
-import Login from './pages/login/login';
+import Login from './pages/login/Login';
 import './styles/global.css'
 import Item from './pages/item/item';
 
